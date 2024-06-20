e8c0c22ca868ed299d6b41bd91481f0a4e01d4c6
Shabet <pakjunyoung@nate.com>
2024-06-20 19:51:46 +0900

    section03(decorator) 내용 추가

---
+++ src/main/java/section03/decorator/starbuzz/CondimentDecorator.java
@@ -0,0 +1,6 @@
+package section03.decorator.starbuzz;
+
+public abstract class CondimentDecorator extends Beverage {
+    Beverage beverage;
+    public abstract String getDescription();
+}
---
+++ src/main/java/section03/decorator/starbuzz/Soy.java
@@ -0,0 +1,18 @@
+package section03.decorator.starbuzz;
+
+public class Soy extends CondimentDecorator {
+
+    public Soy(Beverage beverage) {
+        this.beverage = beverage;
+    }
+
+    @Override
+    public String getDescription() {
+        return beverage.getDescription() + ", Soy";
+    }
+
+    @Override
+    public double cost() {
+        return .15 + beverage.cost();
+    }
+}
